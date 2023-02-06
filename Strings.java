d9c12e42d555fab569d9ba274c43447ff72cfc9b
trigya7701 <rwikroy123@gmail.com>
2023-02-06 13:11:15 +0530

    Strings in java

---
+++ Strings.java
@@ -0,0 +1,49 @@
+public class Strings {
+
+    public static void main(String[] args) {
+        
+        // String s="Apple";
+
+        // StringBuilder s1= new StringBuilder("Apple");
+
+        // System.out.println(s);
+        // System.out.println(s1);
+
+        // s.concat("US");
+        // s1.append("US");
+
+        // System.out.println(s);
+        // System.out.println(s1);
+        // System.out.println();
+        // s=s.concat("US");
+        // s+="123";
+        // System.out.println(s);
+
+        String s1="abc";
+        String s2="abc";
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(s1==s2);
+        // s1="pqr";
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(s1==s2);
+       
+
+        String s3= new String("abc");
+        String s4=new String("abc");
+
+        // System.out.println(s1==s2);
+        // System.out.println(s3==s4);
+        // System.out.println(s1==s3);
+        // System.out.println(s1.equals(s4));
+        
+        String s5="abciii";
+        String s6= s5+"iii";
+
+        System.out.println(s5==s6);
+
+
+    }
+    
+}
